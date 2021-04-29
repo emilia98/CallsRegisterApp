00857caefb19226fc20c161c77a3b292ecdd1137
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2021-04-29 13:03:50 +0300

    renamed some ViewControllers

--- CallsApp/CallsApp/Controllers/DialViewController.swift
+++ CallsApp/CallsApp/Controllers/DialViewController.swift
@@ -1,20 +1,11 @@
 import UIKit
 
 class DialViewController: UIViewController {
-    @IBOutlet var endCall: UIButton!
-    @IBOutlet var callerView: UIView!
-    @IBOutlet var muteButton: UIButton!
-    @IBOutlet var keypadButton: UIButton!
-    @IBOutlet var speakersButton: UIButton!
-    @IBOutlet var addCallButton: UIButton!
-    @IBOutlet var faceTimeButton: UIButton!
-    @IBOutlet var contactsButton: UIButton!
-    @IBOutlet var buttonsView: UIView!
-    @IBOutlet var hideKeypadButton: UIButton!
-    
+    @IBOutlet var callButton: UIButton!
+    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet var addNumberButton: UIButton!
+    @IBOutlet var clearSymbolButton: UIButton!
     var numberPadView: NumberPadView? = nil
-    var dialLabelView: UIView!
-    var dialLabel: UILabel!
     var buttons: [String: UIButton] = [:]
     var zeroPressingTimer = Timer()
     var seconds = 0.0
@@ -22,60 +13,50 @@ class DialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        endCall.setCircleButton()
-        muteButton.setCircleButton()
-        keypadButton.setCircleButton()
-        speakersButton.setCircleButton()
-        addCallButton.setCircleButton()
-        faceTimeButton.setCircleButton()
-        contactsButton.setCircleButton()
-        
-        initDialViewLabel()
+        callButton.imageView?.contentMode = .scaleAspectFit
+        callButton.setCircleButton()
         
         numberPadView = NumberPadView.init()
         numberPadView?.loadView()
         view.addSubview(numberPadView!)
-        buttons = numberPadView!.getButtons()
-        addKeypadButtonsTargets()
-        
-        callerView.isHidden = false
-        dialLabelView.isHidden = true
-        hideKeypadButton.isHidden = true
-        numberPadView?.isHidden = true
-        
-        keypadButton.addTarget(self, action: #selector(hideButtonsView(_:)), for: .touchUpInside)
-        hideKeypadButton.addTarget(self, action: #selector(hideKeyboardPad(_:)), for: .touchDown)
-    }
-    
-    @objc
-    func hideButtonsView(_ sender: UIButton) {
-        buttonsView.isHidden = true
-        hideKeypadButton.isHidden = false
-        numberPadView?.isHidden = false
         
         NSLayoutConstraint.activate([
-            endCall.topAnchor.constraint(equalTo: numberPadView!.bottomAnchor, constant: 50),
+            numberPadView!.topAnchor.constraint(equalTo: addNumberButton.bottomAnchor, constant: 20),
+            callButton.topAnchor.constraint(equalTo: numberPadView!.bottomAnchor, constant: 30),
             numberPadView!.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             numberPadView!.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+        buttons = numberPadView!.getButtons()
+        addKeypadButtonsTargets()
+        
+        numberLabel.text = ""
+        addNumberButton.isHidden = true
+        clearSymbolButton.isHidden = true
+        clearSymbolButton.addTarget(self, action: #selector(clearSymbolButtonPressed(_:)), for: .touchDown)
+        styleClearButton()
     }
     
     @objc
-    func hideKeyboardPad(_ sender: UIButton) {
-        buttonsView.isHidden = false
-        hideKeypadButton.isHidden = true
-        callerView.isHidden = false
-        dialLabelView.isHidden = true
-        
-        dialLabel.text = ""
-        
-        if let numberPad = numberPadView {
-            numberPad.isHidden = true
+    func numericButtonPressed(_ sender: UIButton) {
+        let buttonText = sender.titleLabel?.text
+        var text = buttonText?.components(separatedBy: "\n")[0]
+        if text == "﹡" {
+            text = "*"
         }
+        numberLabel.text = "\(numberLabel.text!)\(text!)"
+        
+        if !numberLabel.text!.isEmpty {
+            addNumberButton.isHidden = false
+            clearSymbolButton.isHidden = false
+        }
+        
+        sender.backgroundColor = UIColor(hex: "#8A8A8A")
     }
     
     private func addKeypadButtonsTargets() {
         buttons.forEach { (key, button) in
+            button.setBackgroundColor(UIColor(hex: "#E1E1E1")!, for: .normal)
+            
             if key == "0" {
                 button.addTarget(self, action: #selector(zeroButtonPressed(_:)), for: .touchDown)
                 button.addTarget(self, action: #selector(zeroButtonReleased(_:)), for: [.touchUpInside, .touchUpOutside])
@@ -85,52 +66,10 @@ class DialViewController: UIViewController {
         }
     }
     
-    private func initDialViewLabel() {
-        dialLabelView = UIView()
-        dialLabelView.translatesAutoresizingMaskIntoConstraints = false
-        view.insertSubview(dialLabelView, aboveSubview: callerView)
-        
-        NSLayoutConstraint.activate([
-            dialLabelView.heightAnchor.constraint(equalTo: callerView.heightAnchor, constant: -20),
-            dialLabelView.widthAnchor.constraint(equalTo: callerView.widthAnchor),
-            dialLabelView.centerXAnchor.constraint(equalTo: callerView.centerXAnchor),
-            dialLabelView.centerYAnchor.constraint(equalTo: callerView.centerYAnchor)
-        ])
-        
-        initDialLabel()
-    }
-    
-    private func initDialLabel() {
-        dialLabel = UILabel()
-        dialLabel.translatesAutoresizingMaskIntoConstraints = false
-        dialLabel.text = ""
-        dialLabel.font = UIFont.systemFont(ofSize: 38)
-        dialLabel.textColor = UIColor.white
-        dialLabelView.addSubview(dialLabel)
-        NSLayoutConstraint.activate([
-            dialLabel.centerYAnchor.constraint(equalTo: dialLabelView.centerYAnchor),
-            dialLabel.centerXAnchor.constraint(equalTo: dialLabelView.centerXAnchor)
-        ])
-    }
-    
-    @objc
-    func numericButtonPressed(_ sender: UIButton) {
-        let buttonText = sender.titleLabel?.text
-        var text: String! = buttonText?.components(separatedBy: "\n")[0]
-        
-        if text == "﹡" {
-            text = "*"
-        }
-        dialLabel.text = "\(dialLabel.text!)\(text!)"
-        
-        if !dialLabel.text!.isEmpty {
-            dialLabelView.isHidden = false
-            callerView.isHidden = true
-        }
-    }
-
     @objc
     func zeroButtonPressed(_ sender: UIButton) {
+        sender.backgroundColor = UIColor(hex: "#8A8A8A")
+        
         zeroPressingTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
@@ -138,7 +77,7 @@ class DialViewController: UIViewController {
     func zeroButtonReleased(_ sender: UIButton) {
         zeroPressingTimer.invalidate()
         let text = seconds >= 1 ? "+" : "0"
-        dialLabel.text = "\(dialLabel.text!)\(text)"
+        numberLabel.text = "\(numberLabel.text!)\(text)"
         seconds = 0
     }
     
@@ -147,14 +86,35 @@ class DialViewController: UIViewController {
         seconds = seconds + 0.01
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc
+    func clearSymbolButtonPressed(_ sender: UIButton) {
+        numberLabel.text?.removeLast()
+        
+        if numberLabel.text!.isEmpty {
+            addNumberButton.isHidden = true
+            clearSymbolButton.isHidden = true
+        }
     }
-    */
+    
+    private func styleClearButton() {
+        let image = UIImage(systemName: "delete.left.fill")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .center
 
+        imageView.preferredSymbolConfiguration = .init(pointSize: 25)
+        imageView.tintColor = UIColor(hex: "#E1E1E1")
+        
+        let buttonImage = UIImage(systemName: "delete.left")
+        clearSymbolButton.setImage(buttonImage, for: .normal)
+        clearSymbolButton.tintColor = .black
+        
+        clearSymbolButton.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.widthAnchor.constraint(equalTo: clearSymbolButton.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: clearSymbolButton.heightAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: clearSymbolButton.trailingAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: clearSymbolButton.leadingAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: clearSymbolButton.topAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: clearSymbolButton.centerYAnchor).isActive = true
+    }
 }
---
+++ CallsApp/CallsApp/Controllers/KeypadViewController.swift
@@ -0,0 +1,160 @@
+import UIKit
+
+class KeypadViewController: UIViewController {
+    @IBOutlet var endCall: UIButton!
+    @IBOutlet var callerView: UIView!
+    @IBOutlet var muteButton: UIButton!
+    @IBOutlet var keypadButton: UIButton!
+    @IBOutlet var speakersButton: UIButton!
+    @IBOutlet var addCallButton: UIButton!
+    @IBOutlet var faceTimeButton: UIButton!
+    @IBOutlet var contactsButton: UIButton!
+    @IBOutlet var buttonsView: UIView!
+    @IBOutlet var hideKeypadButton: UIButton!
+    
+    var numberPadView: NumberPadView? = nil
+    var dialLabelView: UIView!
+    var dialLabel: UILabel!
+    var buttons: [String: UIButton] = [:]
+    var zeroPressingTimer = Timer()
+    var seconds = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        endCall.setCircleButton()
+        muteButton.setCircleButton()
+        keypadButton.setCircleButton()
+        speakersButton.setCircleButton()
+        addCallButton.setCircleButton()
+        faceTimeButton.setCircleButton()
+        contactsButton.setCircleButton()
+        
+        initDialViewLabel()
+        
+        numberPadView = NumberPadView.init()
+        numberPadView?.loadView()
+        view.addSubview(numberPadView!)
+        buttons = numberPadView!.getButtons()
+        addKeypadButtonsTargets()
+        
+        callerView.isHidden = false
+        dialLabelView.isHidden = true
+        hideKeypadButton.isHidden = true
+        numberPadView?.isHidden = true
+        
+        keypadButton.addTarget(self, action: #selector(hideButtonsView(_:)), for: .touchUpInside)
+        hideKeypadButton.addTarget(self, action: #selector(hideKeyboardPad(_:)), for: .touchDown)
+    }
+    
+    @objc
+    func hideButtonsView(_ sender: UIButton) {
+        buttonsView.isHidden = true
+        hideKeypadButton.isHidden = false
+        numberPadView?.isHidden = false
+        
+        NSLayoutConstraint.activate([
+            endCall.topAnchor.constraint(equalTo: numberPadView!.bottomAnchor, constant: 50),
+            numberPadView!.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            numberPadView!.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    @objc
+    func hideKeyboardPad(_ sender: UIButton) {
+        buttonsView.isHidden = false
+        hideKeypadButton.isHidden = true
+        callerView.isHidden = false
+        dialLabelView.isHidden = true
+        
+        dialLabel.text = ""
+        
+        if let numberPad = numberPadView {
+            numberPad.isHidden = true
+        }
+    }
+    
+    private func addKeypadButtonsTargets() {
+        buttons.forEach { (key, button) in
+            if key == "0" {
+                button.addTarget(self, action: #selector(zeroButtonPressed(_:)), for: .touchDown)
+                button.addTarget(self, action: #selector(zeroButtonReleased(_:)), for: [.touchUpInside, .touchUpOutside])
+                return
+            }
+            button.addTarget(self, action: #selector(numericButtonPressed(_:)), for: .touchDown)
+        }
+    }
+    
+    private func initDialViewLabel() {
+        dialLabelView = UIView()
+        dialLabelView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(dialLabelView, aboveSubview: callerView)
+        
+        NSLayoutConstraint.activate([
+            dialLabelView.heightAnchor.constraint(equalTo: callerView.heightAnchor, constant: -20),
+            dialLabelView.widthAnchor.constraint(equalTo: callerView.widthAnchor),
+            dialLabelView.centerXAnchor.constraint(equalTo: callerView.centerXAnchor),
+            dialLabelView.centerYAnchor.constraint(equalTo: callerView.centerYAnchor)
+        ])
+        
+        initDialLabel()
+    }
+    
+    private func initDialLabel() {
+        dialLabel = UILabel()
+        dialLabel.translatesAutoresizingMaskIntoConstraints = false
+        dialLabel.text = ""
+        dialLabel.font = UIFont.systemFont(ofSize: 38)
+        dialLabel.textColor = UIColor.white
+        dialLabelView.addSubview(dialLabel)
+        NSLayoutConstraint.activate([
+            dialLabel.centerYAnchor.constraint(equalTo: dialLabelView.centerYAnchor),
+            dialLabel.centerXAnchor.constraint(equalTo: dialLabelView.centerXAnchor)
+        ])
+    }
+    
+    @objc
+    func numericButtonPressed(_ sender: UIButton) {
+        let buttonText = sender.titleLabel?.text
+        var text: String! = buttonText?.components(separatedBy: "\n")[0]
+        
+        if text == "﹡" {
+            text = "*"
+        }
+        dialLabel.text = "\(dialLabel.text!)\(text!)"
+        
+        if !dialLabel.text!.isEmpty {
+            dialLabelView.isHidden = false
+            callerView.isHidden = true
+        }
+    }
+
+    @objc
+    func zeroButtonPressed(_ sender: UIButton) {
+        zeroPressingTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc
+    func zeroButtonReleased(_ sender: UIButton) {
+        zeroPressingTimer.invalidate()
+        let text = seconds >= 1 ? "+" : "0"
+        dialLabel.text = "\(dialLabel.text!)\(text)"
+        seconds = 0
+    }
+    
+    @objc
+    func updateTimer() {
+        seconds = seconds + 0.01
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
